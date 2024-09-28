edfc5ecf99b2d98a04fb93056f1308a767b4a448
Cyril LM <clevymar@example.com>
2024-09-28 16:59:05 +0200

    small formatting changes

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 from common import dirFiles
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="CLM Environment",page_icon="🌍")
 
 st.title("Environment data explorer")
 st.write('Developped by CLM, first release Sep 2024')
